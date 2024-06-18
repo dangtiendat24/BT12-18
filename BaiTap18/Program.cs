4e5fb80d667e7e554a9adcdc9f2410d7198bb5e2
dangtiendat24 <148854633+dangtiendat24@users.noreply.github.com>
2024-06-18 18:18:43 +0700

    Add files via upload

---
+++ BaiTap18/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace BaiTap18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            long x = readLongFromCosole();
+            long y = (long)Math.Sqrt(x);
+            Console.WriteLine("Căn bậc 2 của số bạn đã nhập là: " + y);
+        }
+        static long readLongFromCosole()
+        {
+            long result;
+            while (true)
+            {
+                Console.Write("Nhập vào số thực 8byte: ");
+                string input = Console.ReadLine();
+                if (long.TryParse(input, out result))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Số bạn nhập không hợp lệ. Nhập lại");
+                }
+            }
+            return result;
+        }
+    }
+}
